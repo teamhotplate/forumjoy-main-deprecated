8319c807b02b7f3062cbbf8a06f7d5d988d023bc
Adrian Schmitz <scegad@scegad.io>
2018-02-25 12:50:01 -0500

    Working authentication and user registration endpoints, with example protected and unprotected api endpoints.

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 import passport from 'passport';
 import passportJWT from 'passport-jwt';
-
 import { User } from '../models';
 
 const ExtractJwt = passportJWT.ExtractJwt;
@@ -14,17 +13,18 @@ jwtOptions.secretOrKey = process.env.JWTSECRET;
 const jwtStrategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
   console.log('payload received', jwt_payload);
   // TO DO (Adrian)
-  // db.User.findOne({where: {id: jwt_payload.user_id}}).then( function(user) {
-  //   console.log(JSON.stringify(user));
-  //   if (!user) {
-  //     console.log("In invalid creds handler. User was null.");
-  //     return next(null, false, { message: 'Invalid user credentials.' });
-  //   } else {
-  //     console.log("In jwt success handler. User was not null.");
-  //     return next(null, user);
-  //   }
-  // });
-  return next(null, false, { message: 'Auth not yet implemented.' });
+  User.findOne({ _id: jwt_payload.user_id }).then((userData, err) => {
+    console.log(userData);
+    if (!userData) {
+      console.log("In invalid creds handler. User was null.");
+      return next(null, false, { message: 'Invalid user credentials.' });
+    } else {
+      console.log("In jwt success handler. User was not null.");
+      return next(null, userData);
+    }
+  }).catch((err) => {
+    return next(null, false, { message: 'Unknown error validating user token.' });
+  });
 });
 
 passport.use(jwtStrategy);
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,15 @@
-//import farmhash from 'farmhash';
-//import request from 'request';
+import passport from 'passport';
 
 import { Router } from 'express';
 
 const router = Router();
 
-router.get("/test", function (req, res) {
-  res.json({"message": "Hello from the /api/test GET endpoint"})
+router.get("/unprotected", function (req, res) {
+  res.json({"message": "Hello from the /api/unprotected GET endpoint"});
+});
+
+router.get("/protected", passport.authenticate('jwt', { session: false }), function (req, res) {
+  res.json({"message": "Hello from the /api/protected GET endpoint"});
 });
 
 export default router;
