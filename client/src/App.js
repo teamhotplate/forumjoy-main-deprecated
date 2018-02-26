6d700b813ff90c7c04194e36cb189d8628288e8b
Adrian Schmitz <scegad@scegad.io>
2018-02-25 19:33:04 -0500

    Materialize set up, client routing, landing and login page stubbed out

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import About from './components/About';
+import Header from './components/Header';
+import Login from './components/Login';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={About} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
       </div>
     );
   }
